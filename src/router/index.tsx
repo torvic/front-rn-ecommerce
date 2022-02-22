82ad24b19828306912a37a714ad3d3356a2eb390
torvic <130111@unsaac.edu.pe>
2022-02-22 18:24:05 -0500

    Add router and BottomTabNav component

---
+++ src/router/index.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+// import HomeScreen from '../screens/HomeScreen';
+import BottomTabNav from './BottomTabNav';
+
+const Root = createStackNavigator();
+
+const Router = () => {
+  return (
+    <NavigationContainer>
+      <Root.Navigator screenOptions={{headerShown: false}}>
+        <Root.Screen name="HomeTabs" component={BottomTabNav} />
+      </Root.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Router;
