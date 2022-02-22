882700a1fcf6188638427add9a0d673bdfa8cbe6
torvic <130111@unsaac.edu.pe>
2022-02-22 12:05:51 -0500

    Add HomeScreen and ProductItem component

---
+++ src/screens/HomeScreen/index.tsx
@@ -0,0 +1,27 @@
+import {View, StyleSheet, FlatList} from 'react-native';
+import React from 'react';
+import ProductItem from '../../components/ProductItem';
+
+import products from '../../data/products';
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.page}>
+      {/* Render product component */}
+      <FlatList
+        data={products}
+        renderItem={({item}) => <ProductItem item={item} />}
+        keyExtractor={item => item.id}
+        showsVerticalScrollIndicator={false}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  page: {
+    padding: 10,
+  },
+});
+
+export default HomeScreen;
