2f5e33892470da5f8f327463b7ccc03434db4ec5
torvic <130111@unsaac.edu.pe>
2022-03-01 13:05:13 -0500

    Add get all products to HomeScreen

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,19 +1,45 @@
-import {View, StyleSheet, FlatList} from 'react-native';
+import {View, StyleSheet, FlatList, ActivityIndicator} from 'react-native';
 import React from 'react';
 import ProductItem from '../../components/ProductItem';
+import {gql, useQuery} from '@apollo/client';
 
-import products from '../../data/products';
+// import products from '../../data/products';
+
+const ALL_PRODUCTS = gql`
+  query AllProducts {
+    allProducts {
+      _id
+      title
+      description
+      image
+      images
+      options
+      avgRating
+      ratings
+      price
+      oldPrice
+      createdAt
+      updatedAt
+    }
+  }
+`;
 
 const HomeScreen = ({searchValue}: {searchValue: string}) => {
+  const {data, loading} = useQuery(ALL_PRODUCTS);
   console.log(searchValue);
+  // console.log('products', data.allProducts);
+
+  if (loading) {
+    return <ActivityIndicator />;
+  }
 
   return (
     <View style={styles.page}>
       {/* Render product component */}
       <FlatList
-        data={products}
+        data={data.allProducts}
         renderItem={({item}) => <ProductItem item={item} />}
-        keyExtractor={item => item.id}
+        keyExtractor={item => item._id}
         showsVerticalScrollIndicator={false}
       />
     </View>
