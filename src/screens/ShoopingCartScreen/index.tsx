fbc9f20fd72e727f9fa8279e02d3c4967e950164
torvic <130111@unsaac.edu.pe>
2022-03-01 13:05:42 -0500

    Add all order items gql to ShoppingCartScreen

--- src/screens/ShoopingCartScreen/index.tsx
+++ src/screens/ShoopingCartScreen/index.tsx
@@ -1,35 +1,80 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, StyleSheet, FlatList, Text} from 'react-native';
 import CartProductItem from '../../components/CartProductItem';
 import {useNavigation} from '@react-navigation/native';
 import Button from '../../components/Button';
+import {gql, useQuery} from '@apollo/client';
 
 import products from '../../data/cart';
 
-const ShopingCartScreen = () => {
-  const navigation = useNavigation();
+const ALL_ORDER_ITEMS = gql`
+  query GetAllOrderItemNotComplete {
+    getAllOrderItemNotComplete {
+      _id
+      quantity
+      option
+      product {
+        _id
+        title
+        image
+        avgRating
+        ratings
+        price
+        oldPrice
+      }
+    }
+  }
+`;
 
-  const totalPrice = products.reduce(
-    (summedPrice, product) =>
-      summedPrice + product.item.price * product.quantity,
-    0,
-  );
+const ShopingCartScreen = () => {
+  const {data, loading} = useQuery(ALL_ORDER_ITEMS);
+  const navigation = useNavigation();
+  const [dbOrderItem, setDbOrderItem] = useState(null);
+
+  // console.log('response ', response);
+
+  useEffect(() => {
+    if (data) {
+      setDbOrderItem(data.getAllOrderItemNotComplete);
+    }
+  }, [data]);
+
+  console.log('data', data);
 
   const onCheckout = () => {
     navigation.navigate('Address');
   };
 
+  if (loading) {
+    return (
+      <View>
+        <Text>Cargando...</Text>
+      </View>
+    );
+  }
+  const totalPrice = data.getAllOrderItemNotComplete.reduce(
+    (summedPrice, product) =>
+      summedPrice + product.product.price * product.quantity,
+    0,
+  );
+
   return (
     <View style={styles.page}>
       {/* Render Product Componet */}
       <FlatList
-        data={products}
-        renderItem={({item}) => <CartProductItem cartItem={item} />}
+        data={dbOrderItem}
+        renderItem={({item}) => (
+          <CartProductItem
+            cartItem={item}
+            dbOrderItem={dbOrderItem}
+            setDbOrderItem={setDbOrderItem}
+          />
+        )}
         showsVerticalScrollIndicator={false}
         ListHeaderComponent={() => (
           <View>
             <Text style={{fontSize: 18}}>
-              Subtotal ({products.length} items):{' '}
+              Subtotal ({dbOrderItem && dbOrderItem.length} items):{' '}
               <Text style={{color: '#e47911', fontWeight: 'bold'}}>
                 ${totalPrice.toFixed(2)}
               </Text>
