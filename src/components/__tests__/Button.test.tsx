0e99832d36dd1d248bfa5ccccecf31e75730d3ff
torvic <130111@unsaac.edu.pe>
2022-03-07 18:35:43 -0500

    Add jest & react native testing library

---
+++ src/components/__tests__/Button.test.tsx
@@ -0,0 +1,9 @@
+import 'react-native';
+import React from 'react';
+import Button from '../Button';
+
+import {render} from '@testing-library/react-native';
+
+it('renders correctly', () => {
+  render(<Button />);
+});
